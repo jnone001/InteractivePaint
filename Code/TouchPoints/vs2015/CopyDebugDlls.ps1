dcfaec8dde390cb21366e7bc72556125bd8206ac
Darkusaido <jnone001@fiu.edu>
2016-05-23 01:51:35 -0400

    scripts now create debug/realease folder if they dont already exist

--- Code/TouchPoints/vs2015/CopyDebugDlls.ps1
+++ Code/TouchPoints/vs2015/CopyDebugDlls.ps1
@@ -1,3 +1,7 @@
-﻿Copy-Item .\dependencies\LeapSDK\lib\x86\Leap.dll Debug -Force -Verbose
-Copy-Item .\dependencies\libusb-1.0.20\MS32\dll\libusb-1.0.dll Debug -Force -Verbose
-Copy-Item .\dependencies\TobiiEyeXSDK_1.7.480\lib\x86\Tobii.EyeX.Client.dll Debug -Force -Verbose
+﻿$debugFolder = ".\Debug\"
+
+if (!(Test-Path -path $debugFolder)) {New-Item $debugFolder -Type Directory}
+
+Copy-Item .\dependencies\LeapSDK\lib\x86\Leap.dll .\Debug\ -Force -Verbose
+Copy-Item .\dependencies\libusb-1.0.20\MS32\dll\libusb-1.0.dll .\Debug\ -Force -Verbose
+Copy-Item .\dependencies\TobiiEyeXSDK_1.7.480\lib\x86\Tobii.EyeX.Client.dll .\Debug\ -Force -Verbose
--- Code/TouchPoints/vs2015/CopyReleaseDlls.ps1
+++ Code/TouchPoints/vs2015/CopyReleaseDlls.ps1
@@ -1,3 +1,7 @@
-﻿Copy-Item .\dependencies\LeapSDK\lib\x86\Leap.dll Release -Force -Verbose
-Copy-Item .\dependencies\libusb-1.0.20\MS32\dll\libusb-1.0.dll Release -Force -Verbose
-Copy-Item .\dependencies\TobiiEyeXSDK_1.7.480\lib\x86\Tobii.EyeX.Client.dll Release -Force -Verbose
+﻿$releaseFolder = ".\Release\"
+
+if (!(Test-Path -path $releaseFolder)) {New-Item $releaseFolder -Type Directory}
+
+Copy-Item .\dependencies\LeapSDK\lib\x86\Leap.dll .\Release\ -Force -Verbose
+Copy-Item .\dependencies\libusb-1.0.20\MS32\dll\libusb-1.0.dll .\Release\ -Force -Verbose
+Copy-Item .\dependencies\TobiiEyeXSDK_1.7.480\lib\x86\Tobii.EyeX.Client.dll .\Release\ -Force -Verbose
